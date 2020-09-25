ea3ea0848f76a773922abad4fc48e8c72c36ea10
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-25 20:35:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? 
+  array.any? do |element|
+    element.length > 4 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if 
 end
