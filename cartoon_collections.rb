c1b7606b102515adb62328d68deabdc012b26d9d
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-25 20:31:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,12 @@ end
 
 def summon_captain_planet(array)
   array.collect do |element|
-    element.capitalize
+    element.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any
 end
 
 def find_the_cheese# code an argument here
